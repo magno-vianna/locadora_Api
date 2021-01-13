30c377c061b0e14e3aba160c90009d736f14c72d
Magno Vianna <mvianna@live.com>
2021-01-13 00:48:34 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes/routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('🚀 Server started at http://localhost:3333');
+});
