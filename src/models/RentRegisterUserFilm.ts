004ec4849a1877db3dd8d09e3b9e0c4ec2acd2d1
Magno Vianna <mvianna@live.com>
2021-01-24 13:42:14 -0300

    Adcionado RentRegisterUserFilm.ts na pasta models

---
+++ src/models/RentRegisterUserFilm.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Long, CreateDateColumn, ManyToMany } from 'typeorm'
+import Film from './Film'
+import User from './User'
+
+@Entity('rent_registers_users_films')
+class RentRegisterUserFilm {
+  @PrimaryGeneratedColumn('uuid')
+  id: Long
+
+  @ManyToMany(() => User, user => user.name)
+  user: User;
+
+  @ManyToMany(() => Film, film => film.title)
+  title: Film;
+
+  @CreateDateColumn({ name: 'lease_start_at' })
+  leaseStartAt: Date;
+
+  @CreateDateColumn({ name: 'lease_end_at' })
+  leaseEndAt: Date;
+
+  @CreateDateColumn({ name: 'film_delivered_at' })
+  filmDeliveredAt: Date;
+}
+export default RentRegisterUserFilm
