807eb5d5f9f6313c575b204a5a5f6698442fc5c5
Magno Vianna <mvianna@live.com>
2021-01-24 13:41:21 -0300

    Adcionado Film.ts na pasta models

---
+++ src/models/Film.ts
@@ -0,0 +1,43 @@
+/* eslint-disable camelcase */
+import { Entity, PrimaryGeneratedColumn, Column, Long, ManyToOne, ManyToMany } from 'typeorm'
+
+import Director from './Director'
+import Genre from './Genre'
+import RentRegistersUsersFilms from './RentRegisterUserFilm'
+
+export type AvailableRoleType = 'available' |'unavailable';
+
+@Entity('films')
+class Film {
+  @PrimaryGeneratedColumn('uuid')
+  id: Long;
+
+  @ManyToOne(() => Director, director => director.films)
+  director: Director;
+
+  @Column()
+  title: string;
+
+  @Column({
+    name: 'lease_cost',
+    type: 'decimal',
+    precision: 5,
+    scale: 2,
+    default: 0
+  })
+  leaseCost: number;
+
+  @ManyToOne(() => Genre, genre => genre.films)
+  genre: Genre;
+
+  @Column({
+    type: 'enum',
+    enum: ['available', 'unavailable'],
+    default: 'available'
+  })
+  availability: AvailableRoleType;
+
+  @ManyToMany(() => RentRegistersUsersFilms, rent_registers_users_films => rent_registers_users_films.title)
+  id_title: RentRegistersUsersFilms
+}
+export default Film
