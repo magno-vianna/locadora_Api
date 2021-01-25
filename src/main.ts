48d8795e6cc8af70bb940365db0a2c4e19b0e22b
Magno Vianna <mvianna@live.com>
2021-01-25 01:10:27 -0300

    Instalaçao, bcrypt e jwt

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import "reflect-metadata";
+import 'reflect-metadata'
 
-import express from 'express';
-import './database/connect';
-import routes from './routes/routes';
+import express from 'express'
+import './database/connect'
+import routes from './routes/users.routes'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(routes);
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
-  console.log('🚀 Server started at http://localhost:3333');
-});
+  console.log('🚀 Server started at http://localhost:3333')
+})
