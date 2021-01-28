fcbfe4b08d75b2a4f7d589caa28e5f74c32a04cb
Magno Vianna <mvianna@live.com>
2021-01-27 22:51:04 -0300

    UserController, create, index, findById e delete implementado

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -18,7 +18,39 @@ class UserController {
     const userCreate = repository.create({ name, email, password, blocked: false })
     await repository.save(userCreate)
 
-    return res.json(userCreate)
+    return res.status(201).json(userCreate)
+  }
+
+  async index (req: Request, res: Response) {
+    const repository = getRepository(User)
+
+    const userListAll = await repository.find()
+
+    res.json(userListAll)
+  }
+
+  async findById (req: Request, res: Response) {
+    const repository = getRepository(User)
+
+    const userId = req.params.id
+
+    const userFindId = await repository.findByIds(userId)
+
+    if (userFindId.length === 0) {
+      return res.sendStatus(409)
+    }
+
+    return res.status(200).json(userFindId)
+  }
+
+  async delete (req: Request, res: Response) {
+    const repository = getRepository(User)
+
+    const userId = req.params.id
+
+    const deleteUser = await repository.delete(userId)
+
+    return res.status(204).json(deleteUser)
   }
 }
 export default UserController
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 
 import UserController from '../controllers/UserController'
+import authMiddleware from '../middlewares/authMiddleware'
 
 const router = Router()
 const userController = new UserController()
 
 router.post('/users', userController.create)
+router.get('/users', userController.index)
+router.get('/users/:id', userController.findById)
+router.delete('/users/:id', userController.delete)
 
 export default router
